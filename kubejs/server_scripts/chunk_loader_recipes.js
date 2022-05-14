d66ef9ea667d1eb9736e32159b9331651d0d4bd2
Michael Pena-Knight <70152741+Mikeatron-User@users.noreply.github.com>
2022-05-14 19:54:51 -0400

    0.59 Hotfix

--- kubejs/server_scripts/chunk_loader_recipes.js
+++ kubejs/server_scripts/chunk_loader_recipes.js
@@ -9,8 +9,6 @@ console.info('Hello, World! (You will see this line every time server resources 
 
 onEvent('recipes', event => {
 
-  //Replace Recipes for Chunk Loaders
-  event.replaceInput({id: 'minecraft:end_crystal'}, 'outer_end:rose_crystal', 'minecraft:ender_eye')
   
   event.remove({id: 'chunkloaders:basic_chunk_loader'})
   event.shaped('1x chunkloaders:basic_chunk_loader', [
@@ -19,7 +17,7 @@ onEvent('recipes', event => {
     'BEB'
   ], {
     E: 'minecraft:ender_eye',
-	C: 'minecraft:wither_star',
+	C: 'minecraft:nether_star',
 	B: 'quark:biotite'
   })
 })
