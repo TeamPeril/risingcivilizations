02c5b8c74fc6dcc8bf4a47fa23aaefae9ccd2786
Michael Pena-Knight <70152741+Mikeatron-User@users.noreply.github.com>
2022-12-27 20:05:59 -0500

    1.0-Beta-0.3
    Fixed: Bug where game would crash if refined storage block got blown up.
    Changed: Netherwart blocks have a chance to drop crimson fungus.
    Changed: The Grand Market. Now better organized and segmented.
    Changed: Grand Market now has shopkeeper images.
    Changed: Textures for Mekanism ores.
    Changed: Textures for Mekanism ores.
    Removed: Overlapping elements from main menu.

--- kubejs/client_scripts/tool_tips_scripts.js
+++ kubejs/client_scripts/tool_tips_scripts.js
@@ -13,27 +13,7 @@ onEvent('item.tooltip', tooltip => {
 				'ironchest:dirt_chest']
 				, 'Cannot be used with create contraptions.')
 				
-	tooltip.add(['betterendforge:aeternium_hammer',
-				'betterendforge:iron_hammer',
-				'betterendforge:golden_hammer',
-				'betterendforge:diamond_hammer',
-				'betterendforge:netherite_hammer',
-				'betterendforge:thallasium_hammer',
-				'betterendforge:terminite_hammer',]
-				, 'Used for crafting, not mining.')
-				
-	tooltip.add(['omnis:ravaged_hammer',
-				'omnis:haunted_hammer']
-				, 'A combat hammer, not for mining.')
-				
-	tooltip.add(['stalwart_dungeons:tungsten_hammer',
-				'stalwart_dungeons:nether_hammer',
-				'stalwart_dungeons:wooden_hammer',
-				'stalwart_dungeons:stone_hammer',
-				'stalwart_dungeons:iron_hammer',
-				'stalwart_dungeons:iron_hammer',
-				'stalwart_dungeons:golden_hammer',
-				'stalwart_dungeons:diamond_hammer',
-				'stalwart_dungeons:netherite_hammer']
-				, 'A combat and mining hammer.')
+	tooltip.add(['minecraft:crimson_fungus', 'minecraft:warped_fungus']
+				, 'Needs bonemeal to grow. Can only be grown on netherrack.')
+
 })
